bd9a4b03bc27bb1ce91e5c6f0c20e0b2a2f860fb
Soham Kulkarni <sohamkulkarni.insnapsys@gmail.com>
2025-10-28 12:35:04 +0530

    second

--- src/components/header/LogoutBtn.jsx
+++ src/components/header/LogoutBtn.jsx
@@ -12,9 +12,12 @@ function LogoutBtn() {
     }
   return (
     <button
-    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-    onClick={logoutHandler}
-    >Logout</button>
+  className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+  onClick={logoutHandler}
+>
+  Logout
+</button>
+
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
 import Select from "./Select";
-import Header from "./header/Header";
-import Footer from "./footer/Footer";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBtn";
+import LogoutBtn from "./Header/LogoutBtn.jsx"; // <-- added .jsx
 import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Login";
@@ -12,6 +12,7 @@ import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
 import Input from "./Input";
+
 export {
     Header,
     Footer,
@@ -27,4 +28,4 @@ export {
     AuthLayout, 
     Input,
     Select,
-}
+};
